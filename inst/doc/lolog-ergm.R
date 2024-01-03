202b0aa9532762a919b8dc4af79072e867bd28ff
ifellows <ianfellows@athena.attlocal.net>
2024-01-03 11:54:56 -0800

    added Pavel's change to the ergm code

--- inst/doc/lolog-ergm.R
+++ inst/doc/lolog-ergm.R
@@ -1,4 +1,4 @@
-## ----setup, include = FALSE----------------------------------------------
+## ----setup, include = FALSE---------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>",
@@ -7,7 +7,7 @@ knitr::opts_chunk$set(
   dpi=120
 )
 
-## ---- fig.height=7-------------------------------------------------------
+## ---- fig.height=7------------------------------------------------------------
 suppressPackageStartupMessages(library(network))
 library(lolog)
 data(ukFaculty)
@@ -18,15 +18,15 @@ delete.vertices(ukFaculty, which(is.na(ukFaculty %v% "Group")))
 plot(ukFaculty, vertex.col = (ukFaculty %v% "Group" ) + 1)
 ukFaculty
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 fitukInd <- lolog(ukFaculty ~ edges() + nodeMatch("GroupC") + nodeFactor("GroupC"))
 summary(fitukInd)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 suppressPackageStartupMessages(library(ergm))
 ergm(ukFaculty ~ edges() + nodematch("GroupC") + nodefactor("GroupC", base=3))
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 g <- gofit(fitukInd, ukFaculty ~ degree(0:50,"out"))
 plot(g)
 g <- gofit(fitukInd, ukFaculty ~ degree(0:50,"in"))
@@ -34,16 +34,16 @@ plot(g)
 g <- gofit(fitukInd, ukFaculty ~ esp(0:25))
 plot(g)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 g <- gofit(fitukInd, ukFaculty ~ edges +mutual)
 plot(g,type="box")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 fituk <- lolog(ukFaculty ~ edges() + nodeMatch("GroupC") + nodeFactor("GroupC") + 
             triangles, nsamp=2000, verbose=FALSE)
 summary(fituk)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 g <- gofit(fituk, ukFaculty ~ degree(0:50,"out"))
 plot(g)
 g <- gofit(fituk, ukFaculty ~ degree(0:50,"in"))
@@ -53,10 +53,10 @@ plot(g)
 g <- gofit(fituk, ukFaculty ~ edges + mutual)
 plot(g, type="box")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 plot(fituk)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 fitukErgm <- ergm(ukFaculty ~ edges() + nodematch("GroupC") + nodefactor("GroupC", base=3) +
             gwesp(decay=.25, fixed=TRUE), verbose=FALSE)
 summary(fitukErgm)
